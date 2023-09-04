1452ac40ee9e72740a663256cb9bb0b4119936dd
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2023-09-04 11:30:14 +0700

    add last-colors

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,20 +1,21 @@
 -- Themes
 return {
+	{
+		"raddari/last-color.nvim",
+	},
+
+	"Shatur/neovim-ayu",
 	"typicode/bg.nvim",
-
 	"ellisonleao/gruvbox.nvim",
-
+	"sainnhe/gruvbox-material",
+	"sainnhe/everforest",
+	"savq/melange-nvim",
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 	},
-
 	{
 		"rose-pine/nvim",
 		name = "rose-pine",
 	},
-
-	"sainnhe/everforest",
-
-	"savq/melange-nvim"
 }
