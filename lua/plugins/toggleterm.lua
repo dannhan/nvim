2160b4af681b566024da5f84dc70518e7819f88b
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2023-09-04 09:54:38 +0700

    change winblend value

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,17 +4,16 @@ return {
 		require("toggleterm").setup {
 			-- size can be a number or function which is passed the current terminal
 			open_mapping = [[<c-p>]],
-			shade_filetypes = {},
-			shade_terminals = false,
+			shade_terminals = true,
 			shading_factor = 1,
 			start_in_insert = true,
 			insert_mappings = true,
-			persist_size = true,
+			persist_size = false,
 			direction = 'float',
 			close_on_exit = true,
 			float_opts = {
 				border = 'curved', -- single/double/shadow/curved
-				winblend = 4,
+				winblend = 10,
 			},
 			winbar = {
 				enabled = true,
