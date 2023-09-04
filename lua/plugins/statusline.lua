93ce801cd412426d6fd74f8703a62d193086a2d4
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2023-09-04 11:28:55 +0700

    change separtor_style and minor changes

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -4,28 +4,26 @@ return {
 		"akinsho/bufferline.nvim",
 		dependencies = "nvim-tree/nvim-web-devicons",
 		config = function()
-			require("bufferline").setup({
+			local bufferline = require("bufferline")
+			bufferline.setup({
 				options = {
+					mode = "buffers",
+					style_preset = bufferline.style_preset.no_italic,
+					separator_style = "thick",
 					diagnostics = "nvim_lsp",
-					diagnostics_upadte_in_insert = true,
-					separator_style = "thin",
-					custom_filter = function(buf_number)
-						if vim.bo[buf_number].filetype ~= "qf" then
-							return true
-						end
-						if vim.bo[buf_number].filetype ~= 'oil' then
-							return true
-						end
-					end,
+					diagnostics_update_in_insert = true,
 					offsets = {
-						{
-							filetype = "NvimTree",
-							text = "File Explorer",
-							highlight = "EcovimNvimTreeTitle",
-							text_align = "center",
-							separator = true,
-						},
+						{ filetype = "NvimTree", text = "File Explorer", text_align = "center", separator = true, },
 					},
+				custom_filter = function(buf_number)
+					if vim.bo[buf_number].filetype ~= "qf" then
+							return true
+						end
+					if vim.bo[buf_number].filetype ~= 'oil' then
+							return true
+						end
+						return true
+					end,
 				}
 			})
 		end,
