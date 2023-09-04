cd1c5a354c2289ad96fcb51a1a75bb04ae9acc29
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2023-09-04 11:30:52 +0700

    change color settings to using last-color

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -12,14 +12,10 @@ local opts = {
 	backup = false,
 	undofile = true,
 	termguicolors = true,
-  scrolloff = 8,
+	scrolloff = 8,
 }
 
 -- Set options from table
 for opt, val in pairs(opts) do
 	vim.o[opt] = val
 end
-
--- Set other options
-local colorscheme = require("helpers.colorscheme")
-vim.cmd.colorscheme(colorscheme)
