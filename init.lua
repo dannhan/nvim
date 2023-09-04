c2e73b88152f961d5f5c75d8b10ba512b7300492
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2023-09-04 09:25:32 +0700

    reordering

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 -- General Neovim keymaps
 require("core.keymaps")
 
--- Other options
-require("core.options")
-
 -- Handle plugins with lazy.nvim
 require("core.lazy")
+
+-- Other options
+require("core.options")
 
