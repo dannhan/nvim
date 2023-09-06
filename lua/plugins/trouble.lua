ab7d7e8a7943f10c4d06f2490e7af6f366f4983f
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2023-09-06 12:57:05 +0700

    Add keybindings and some fixing

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,8 +1,19 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("trouble").setup({
+      mode = "document_diagnostics",
+      action_keys = {
+        close = {"q", "m"},
+      },
+      auto_close = true,
+    })
+  end,
   cmd = { "TroubleToggle", "Trouble" },
 	keys = {
 		{"<C-M>", "<cmd>TroubleToggle document_diagnostics<cr>"},
 	},
 }
+-- Border
+-- "none", "single", "double", "rounded", "solid", "shadow"
